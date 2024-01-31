09f94e6b4a7b3d943652f7f529e22c3f8b2c7dda
Noah Clemmensen <nc@maturix.com>
2024-01-31 08:55:48 +0100

    Added layouts

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set Templating Engine
+app.use(expressLayouts);
+app.set('layout', './layouts/full-width');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
